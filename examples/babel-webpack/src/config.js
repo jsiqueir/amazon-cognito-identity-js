a402af44054f3e9c7d7d54d642f86628b9213888
Jorge Siqueira <jorge.siqueira@gmail.com>
2017-04-13 15:01:59 +0200

    update config to QnG data

--- examples/babel-webpack/src/config.js
+++ examples/babel-webpack/src/config.js
@@ -1,6 +1,6 @@
 export default {
-  region: '',
-  IdentityPoolId: '',
-  UserPoolId: '',
-  ClientId: '',
+    region: 'us-east-1',
+    IdentityPoolId: '',
+    UserPoolId: '80855500-ebf7-4fd9-ab37-48e76ac3dcea',
+    ClientId: '',
 }
