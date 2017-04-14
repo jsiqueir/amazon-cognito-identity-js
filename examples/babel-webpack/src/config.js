7191370eef4e1ac3a67d7314e4b161a3a35ec5e5
Jorge Siqueira <jorge.siqueira@gmail.com>
2017-04-14 12:54:04 +0200

    update

--- examples/babel-webpack/src/config.js
+++ examples/babel-webpack/src/config.js
@@ -1,6 +1,6 @@
 export default {
     region: 'us-east-1',
-    IdentityPoolId: '',
-    UserPoolId: '80855500-ebf7-4fd9-ab37-48e76ac3dcea',
-    ClientId: '',
+    IdentityPoolId: '80855500-ebf7-4fd9-ab37-48e76ac3dcea',
+    UserPoolId: 'us-east-1_7Jc7HOg8W',
+    ClientId: 'v9d1c5lf5s8t73hlpsrt6lkon',
 }
